a86b36f448a4eaeda922c89d782819027acff4b2
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-26 17:13:03 -0600

    init + boilerplate

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB1tK8wUvLQrlI28-adQJ89T_Ejn-HWqMA",
+    authDomain: "train-time-b5a49.firebaseapp.com",
+    databaseURL: "https://train-time-b5a49.firebaseio.com",
+    projectId: "train-time-b5a49",
+    storageBucket: "",
+    messagingSenderId: "466541826684",
+    appId: "1:466541826684:web:84e737a8d37614f09feff9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
