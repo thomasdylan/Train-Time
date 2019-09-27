f3a8f3d02ac97f53e24840367f0537baec102f4f
Dylan Thomas <thomasdylan76@gmail.com>
2019-09-26 19:09:17 -0600

    Simple functionality showing trains name and destination only.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,45 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+var currentTime = moment();
+console.log(currentTime._d);
+
+$('#current-time').text(currentTime._d);
+
+$("#submit-train").on("click", function(e) {
+    e.preventDefault();
+
+    var name = $('#train-input').val().trim();
+    var dest = $('#destination-input').val().trim();
+    var time = $('#time-input').val().trim();
+    var freq = $('#frequency-input').val().trim();
+
+    console.log(name);
+    console.log(dest);
+    console.log(time);
+    console.log(freq);
+
+    database.ref().push({
+        name: name,
+        destination: dest,
+        firstTime: time,
+        frequency: freq,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+    $('#input-form').trigger('reset');
+});
+
+database.ref().orderByChild('dateAdded').on("child_added", function(snapshot) {
+    var tr = $('<tr>');
+    var trainName = $('<td>').text(snapshot.val().name);
+    var destination = $('<td>').text(snapshot.val().destination);
+    var frequency = $('<td>').text(snapshot.val().frequency);
+    var nextArrival = $('<td>').text(moment()._d);
+    var minutesAway = $('<td>').text('???');
+    tr.append(trainName, destination, frequency, nextArrival, minutesAway);
+    $('#manage-data').append(tr);
+});
